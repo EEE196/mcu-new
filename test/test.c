33023f173ac561630f68b07f1dfe4d2fd6755f91
unknown <mahikgot@gmail.com>
2023-05-24 00:35:51 +0800

    add co2 drivers

--- test/test.c
+++ test/test.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include <usart.h>
 #include <spi.h>
-#include <tim.h>
 
 #include "fatfs.h"
 #include "../pm2.5/sps30.h"
 #include "../gps/gps.h"
 #include "../lora/rfm95.h"
+#include "../co2/scd30.h"
 
 
 void test_sd( void )
@@ -185,7 +185,69 @@ void test_lora(void) {
 	}
 }
 
+void test_co(void) {
+    float co2_ppm, temperature, relative_humidity;
+    int16_t err;
+    uint16_t interval_in_seconds = 2;
 
+    /* Initialize I2C */
+    sensirion_i2c_init();
+
+    /* Busy loop for initialization, because the main loop does not work without
+     * a sensor.
+     */
+    while (scd30_probe() != NO_ERROR) {
+        printf("SCD30 sensor probing failed\n");
+        sensirion_sleep_usec(1000000u);
+    }
+    printf("SCD30 sensor probing successful\n");
+
+    scd30_set_measurement_interval(interval_in_seconds);
+    sensirion_sleep_usec(20000u);
+    scd30_start_periodic_measurement(0);
+
+    while (1) {
+        uint16_t data_ready = 0;
+        uint16_t timeout = 0;
+
+        /* Poll data_ready flag until data is available. Allow 20% more than
+         * the measurement interval to account for clock imprecision of the
+         * sensor.
+         */
+        for (timeout = 0; (100000 * timeout) < (interval_in_seconds * 1200000);
+             ++timeout) {
+            err = scd30_get_data_ready(&data_ready);
+            if (err != NO_ERROR) {
+                printf("Error reading data_ready flag: %i\n", err);
+            }
+            if (data_ready) {
+                break;
+            }
+            sensirion_sleep_usec(100000);
+        }
+        if (!data_ready) {
+            printf("Timeout waiting for data_ready flag\n");
+            continue;
+        }
+
+        /* Measure co2, temperature and relative humidity and store into
+         * variables.
+         */
+        err =
+            scd30_read_measurement(&co2_ppm, &temperature, &relative_humidity);
+        if (err != NO_ERROR) {
+            printf("error reading measurement\n");
+
+        } else {
+            printf("measured co2 concentration: %0.2f ppm, "
+                   "measured temperature: %0.2f degreeCelsius, "
+                   "measured humidity: %0.2f %%RH\n",
+                   co2_ppm, temperature, relative_humidity);
+        }
+    }
+
+    scd30_stop_periodic_measurement();
+}
 
 
 // Create the handle for the RFM95 module.
